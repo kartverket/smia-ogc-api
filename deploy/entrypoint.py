8e63332e0949552c6d20ae1ad8cacb81acd11017
Kristian <kristian.haugerud@kartverket.no>
2026-02-23 12:46:07 +0100

    TS-2747: Add api key (#9)

--- deploy/entrypoint.py
+++ deploy/entrypoint.py
@@ -1,5 +1,18 @@
+import os
+
 from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 from pygeoapi.flask_app import APP as app
+
+API_KEY = os.environ.get("OGC_API_KEY")
+
+OPEN_PATHS = {'/', '/openapi', '/conformance', '/health'}
+
+if API_KEY:
+    @app.before_request
+    def check_api_key():
+        from flask import request, abort
+        if request.path not in OPEN_PATHS and request.headers.get("X-API-Key") != API_KEY:
+            abort(401)
 
 metrics = GunicornPrometheusMetrics(app)
 
