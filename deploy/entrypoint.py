e8d4edc6ff7b0fabf3f68278e4f4c34d9833319a
Kristian <kristian.haugerud@kartverket.no>
2026-02-23 10:28:02 +0100

    Health endepunkt

--- deploy/entrypoint.py
+++ deploy/entrypoint.py
@@ -2,3 +2,9 @@ from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 from pygeoapi.flask_app import APP as app
 
 metrics = GunicornPrometheusMetrics(app)
+
+
+@app.route('/health')
+@metrics.do_not_track()
+def health():
+    return '', 200
