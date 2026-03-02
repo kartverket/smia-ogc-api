08e23712502e6e1852b3056165de278270415c78
Kristian <kristian.haugerud@kartverket.no>
2026-03-02 07:06:55 +0100

    config

--- run_local.sh
+++ run_local.sh
@@ -3,6 +3,7 @@ unset VIRTUAL_ENV
 export PYGEOAPI_CONFIG=pygeoapi-config.yml
 export PYGEOAPI_OPENAPI=pygeoapi-openapi.yml
 export PYTHONPATH="${PYTHONPATH:+$PYTHONPATH:}$(dirname "$0")"
+export PYGEOAPI_LOGLEVEL=DEBUG
 
 uv run pygeoapi openapi generate $PYGEOAPI_CONFIG --output-file $PYGEOAPI_OPENAPI
 uv run pygeoapi serve
