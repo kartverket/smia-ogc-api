b6417eee1ed63de07fcba9000909cf69c7508b8c
Kristian <kristian.haugerud@kartverket.no>
2026-03-06 08:13:33 +0100

    TS-2767: Add headere (#17)
    o Legger til header basert på sikkerhetsscan
    o Legger til logger som json slik at vi kan bruke eksisterende grafana
    dashboard. Likt som kommuneinfo og stedsnavn api.

--- deploy/gunicorn_conf.py
+++ deploy/gunicorn_conf.py
@@ -1,4 +1,91 @@
+import datetime
+import logging
+import sys
+
+import json_log_formatter
 from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
+
+accesslog = "-"
+errorlog = "-"
+
+
+class JsonRequestFormatter(json_log_formatter.JSONFormatter):
+    def json_record(
+        self,
+        message: str,
+        extra: dict[str, str | int | float],
+        record: logging.LogRecord,
+    ) -> dict[str, str | int | float]:
+        response_time = datetime.datetime.strptime(
+            record.args["t"], "[%d/%b/%Y:%H:%M:%S %z]"
+        )
+        url = record.args["U"]
+        if record.args["q"]:
+            url += f"?{record.args['q']}"
+
+        return dict(
+            remote_ip=record.args["h"],
+            method=record.args["m"],
+            path=url,
+            status=str(record.args["s"]),
+            time=response_time.isoformat(),
+            user_agent=record.args["a"],
+            referer=record.args["f"],
+            duration_in_ms=record.args["M"],
+            pid=record.args["p"],
+        )
+
+
+class JsonErrorFormatter(json_log_formatter.JSONFormatter):
+    def json_record(
+        self,
+        message: str,
+        extra: dict[str, str | int | float],
+        record: logging.LogRecord,
+    ) -> dict[str, str | int | float]:
+        payload: dict[str, str | int | float] = super().json_record(
+            message, extra, record
+        )
+        payload["level"] = record.levelname
+        return payload
+
+
+logconfig_dict = {
+    "version": 1,
+    "formatters": {
+        "json_request": {
+            "()": JsonRequestFormatter,
+        },
+        "json_error": {
+            "()": JsonErrorFormatter,
+        },
+    },
+    "handlers": {
+        "json_request": {
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "json_request",
+        },
+        "json_error": {
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "json_error",
+        },
+    },
+    "root": {"level": "INFO", "handlers": ["json_error"]},
+    "loggers": {
+        "gunicorn.access": {
+            "level": "INFO",
+            "handlers": ["json_request"],
+            "propagate": False,
+        },
+        "gunicorn.error": {
+            "level": "INFO",
+            "handlers": ["json_error"],
+            "propagate": False,
+        },
+    },
+}
 
 
 def when_ready(server):
