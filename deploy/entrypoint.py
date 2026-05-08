6abd1a8dc5ddce7a3858fadbad7c893adc5a197b
Kristian <haugerud.kristian@gmail.com>
2026-05-08 09:48:57 +0200

    Revert "TS-2787: Fjern API-nøkkel i dev test (#45)"
    This reverts commit 3d93f4442b86427103cac66ba71add43afe5d2a7.

--- deploy/entrypoint.py
+++ deploy/entrypoint.py
@@ -1,6 +1,8 @@
+import hmac
 import logging
 import os
 
+from flask import abort, request
 from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 from pygeoapi.flask_app import APP as app
 
@@ -9,6 +11,20 @@ logger = logging.getLogger(__name__)
 API_KEY = os.environ.get("OGC_API_KEY")
 
 OPEN_PATHS = {"/v1/", "/v1/openapi", "/v1/conformance", "/health"}
+
+
+@app.before_request
+def check_api_key():
+    if request.path in OPEN_PATHS:
+        return
+    if not API_KEY:
+        abort(503)
+    provided = request.headers.get("X-API-Key", "")
+    if not hmac.compare_digest(provided, API_KEY):
+        logger.warning(
+            "Ugyldig API key for %s fra %s", request.path, request.remote_addr
+        )
+        abort(401)
 
 
 @app.after_request
