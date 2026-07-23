e15b5a13afd21cd39a146d36a23f70cc7a501ca4
Vegard Smines <arnedeig@gmail.com>
2026-07-23 10:42:17 +0200

    Legger inn statement timeout med string format for å sikre mot sql injection

--- processes/utils/boplikt_db.py
+++ processes/utils/boplikt_db.py
@@ -52,7 +52,7 @@ def _get_pool():
         try:
             _db_pool = ThreadedConnectionPool(
                 minconn=1,
-                maxconn=int(os.environ.get("DB_POOL_MAX", "10")),
+                maxconn=2,
                 host=os.environ.get("DB_HOST", "localhost"),
                 port=os.environ.get("DB_PORT", "5432"),
                 dbname=os.environ.get("DB_NAME", "postgres"),
@@ -74,7 +74,10 @@ def _execute_query(sql, params):
         try:
             with conn:
                 with conn.cursor() as cur:
-                    cur.execute(f"SET LOCAL statement_timeout = '{_STATEMENT_TIMEOUT}'")
+                    cur.execute(
+                        "SET LOCAL statement_timeout = %s",
+                        (_STATEMENT_TIMEOUT,),
+                    )
                     cur.execute(sql, params)
                     return cur.fetchall()
         except (OperationalError, InterfaceError) as e:
