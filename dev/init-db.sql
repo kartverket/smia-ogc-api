a662214c6fc20f259cfd5de5e8e763ffdb73f2db
Vegard Rognstad Smines <42798416+vegarrsm@users.noreply.github.com>
2026-08-14 08:17:58 +0000

    TS-3053: Legger til lokalId som id felt og kommunenummer som tittel felt (#142)
    # Sjekkliste
    
    - [ ] Har AI blitt brukt til utvikling av denne PR-en?

--- dev/init-db.sql
+++ dev/init-db.sql
@@ -2,7 +2,8 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE SCHEMA IF NOT EXISTS inndelinger;
 
 CREATE TABLE inndelinger.bopliktomraade (
-    kommunenummer                    text PRIMARY KEY,
+    "lokalId"                          text PRIMARY KEY,
+    kommunenummer                    text NOT NULL,
     "gjelderKunDelAvKommunen"        boolean NOT NULL,
     "gjelderForBruktSomHelarsbolig"  boolean,
     "gjelderForBoligIkkeTattIBruk"   boolean,
@@ -16,7 +17,11 @@ CREATE TABLE inndelinger.bopliktomraade (
 CREATE INDEX bopliktomraade_omrade_gix
     ON inndelinger.bopliktomraade USING gist (omrade);
 
+CREATE INDEX bopliktomraade_kommunenummer_idx
+    ON inndelinger.bopliktomraade (kommunenummer);
+
 INSERT INTO inndelinger.bopliktomraade (
+    "lokalId",
     kommunenummer,
     "gjelderKunDelAvKommunen",
     "gjelderForBruktSomHelarsbolig",
@@ -27,15 +32,15 @@ INSERT INTO inndelinger.bopliktomraade (
     "harUsikkerAvgrensning",
     omrade
 ) VALUES (
+    '00000000-0000-0000-0000-000000000001',
     '4601', true, true, true, false, true, NULL, false,
     ST_Multi(ST_GeomFromText(
         'POLYGON((68900 6627300, 69100 6627300, 69100 6627400, 68900 6627400, 68900 6627300))',
         25833))
 );
 
-
--- Full boplikt for hele kommunen
 INSERT INTO inndelinger.bopliktomraade (
+    "lokalId",
     kommunenummer,
     "gjelderKunDelAvKommunen",
     "gjelderForBruktSomHelarsbolig",
@@ -46,6 +51,28 @@ INSERT INTO inndelinger.bopliktomraade (
     "harUsikkerAvgrensning",
     omrade
 ) VALUES (
+    '00000000-0000-0000-0000-000000000002',
+    '4601', true, true, true, false, true, NULL, false,
+    ST_Multi(ST_GeomFromText(
+        'POLYGON((69400 6627300, 69600 6627300, 69600 6627400, 69400 6627400, 69400 6627300))',
+        25833))
+);
+
+
+-- Full boplikt for hele kommunen
+INSERT INTO inndelinger.bopliktomraade (
+    "lokalId",
+    kommunenummer,
+    "gjelderKunDelAvKommunen",
+    "gjelderForBruktSomHelarsbolig",
+    "gjelderForBoligIkkeTattIBruk",
+    "gjelderForUbebygdBoligtomt",
+    "harUnntakFraSlektskapsunntak",
+    "andreLokaleAvgrensninger",
+    "harUsikkerAvgrensning",
+    omrade
+) VALUES (
+    '00000000-0000-0000-0000-000000000003',
     '0301', false, true, true, true, false, NULL, false,
     ST_Multi(ST_GeomFromText(
         'POLYGON((260000 6650000, 261000 6650000, 261000 6651000, 260000 6651000, 260000 6650000))',
@@ -54,6 +81,7 @@ INSERT INTO inndelinger.bopliktomraade (
 
 -- Område med usikker avgrensning
 INSERT INTO inndelinger.bopliktomraade (
+    "lokalId",
     kommunenummer,
     "gjelderKunDelAvKommunen",
     "gjelderForBruktSomHelarsbolig",
@@ -64,6 +92,7 @@ INSERT INTO inndelinger.bopliktomraade (
     "harUsikkerAvgrensning",
     omrade
 ) VALUES (
+    '00000000-0000-0000-0000-000000000004',
     '1806', true, true, false, true, true, 'Avgrensning under revisjon.', true,
     ST_Multi(ST_GeomFromText(
         'POLYGON((599500 7596500, 600500 7596500, 600500 7597500, 599500 7597500, 599500 7596500))',
@@ -72,6 +101,7 @@ INSERT INTO inndelinger.bopliktomraade (
 
 -- Naboområde som deler grense med 4601 i x=69100.
 INSERT INTO inndelinger.bopliktomraade (
+    "lokalId",
     kommunenummer,
     "gjelderKunDelAvKommunen",
     "gjelderForBruktSomHelarsbolig",
@@ -82,6 +112,7 @@ INSERT INTO inndelinger.bopliktomraade (
     "harUsikkerAvgrensning",
     omrade
 ) VALUES (
+    '00000000-0000-0000-0000-000000000005',
     '4602', true, true, false, false, false, NULL, false,
     ST_Multi(ST_GeomFromText(
         'POLYGON((69100 6627300, 69300 6627300, 69300 6627400, 69100 6627400, 69100 6627300))',
